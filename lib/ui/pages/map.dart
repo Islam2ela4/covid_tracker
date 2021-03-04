1e2d0b8082aaed302a08e3b7d6bd1c175f056d14
Islam Qelash <islam2ela4@gmail.com>
2021-03-04 10:31:16 +0200

    final commit

--- lib/ui/pages/map.dart
+++ lib/ui/pages/map.dart
@@ -1,11 +1,9 @@
 
 import 'package:covid_tracker/business_logic/models/country_cases.dart';
-import 'package:covid_tracker/business_logic/view_models/home_screen_viewmodel.dart';
 import 'package:covid_tracker/ui/components/buildui_map.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
 
 class Map_ extends StatelessWidget {
   static final ROUTE = '/map';
